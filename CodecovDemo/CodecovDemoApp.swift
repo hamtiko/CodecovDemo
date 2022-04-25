7d248bbf8841470aafa39dfa751455b6d95c0e12
Tigran Hambardzumyan <tigran.hambardzumyan@picsart.com>
2022-04-25 11:41:19 +0400

    Initial Commit

---
+++ CodecovDemo/CodecovDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodecovDemoApp.swift
+//  CodecovDemo
+//
+//  Created by Tigran Hambardzumyan on 25.04.22.
+//
+
+import SwiftUI
+
+@main
+struct CodecovDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CodecovDemoUITests/CodecovDemoUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  CodecovDemoUITestsLaunchTests.swift
+//  CodecovDemoUITests
+//
+//  Created by Tigran Hambardzumyan on 25.04.22.
+//
+
+import XCTest
+
+class CodecovDemoUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
