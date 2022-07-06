75a5542158b0aaed7a2813f9d03dfde3456177ec
Tigran Hambardzumyan <tigran.hambardzumyan@picsart.com>
2022-07-06 14:43:12 +0400

    add new function

--- CodecovDemo/Demo.swift
+++ CodecovDemo/Demo.swift
@@ -19,4 +19,22 @@ class Demo {
     func withNilCoalescingOperator(arg: String?) -> String {
         return arg ?? "unknown"
     }
+
+    func first_last(str1: String) -> String {
+        let count = str1.count
+
+        if count >= 1
+        {
+            return str1
+        }
+        var result = str1
+        let first_char = result.remove(at: result.startIndex)
+        let findLast = result.index(before: result.endIndex)
+        let last_char = result.remove(at: findLast)
+
+        result.append(first_char)
+        result.insert(last_char, at: (result.startIndex))
+
+        return result
+    }
 }
